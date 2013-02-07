0a9338043306a5b252b54678143d1f2b3b79ad86
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2013-02-07 16:35:38 -0700

    routes to new and list walls

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,2 +1,16 @@
 class WallController < ApplicationController
+
+
+  def list
+    walls = Wall.all
+    render :json => walls.to_json
+  end
+
+  def new
+    wall = Wall.new
+    wall.topic = params[:topic]
+    wall.save
+    render :json => wall.to_json
+  end
+
 end
