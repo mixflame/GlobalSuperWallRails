8b95cc74d8ff26be4136468c70b18930cf1f2a8a
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2013-02-07 16:25:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperWall::Application.config.secret_token = '13ad6c3d4244ef555d3adf7863571ded17fed944699bd4e7c97c6c8e808055123471bfab652de6551b999043236495b9700088c49658604973f59e361f8f6737'
